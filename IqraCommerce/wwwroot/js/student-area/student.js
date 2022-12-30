1f179c179a4b6b70eb2857d49c2247865783cb7d
Md.Moinul hasan Raju <84829433+Raju-cs@users.noreply.github.com>
2022-12-30 14:22:28 +0600

    Add PhoneValidations

--- IqraCommerce/wwwroot/js/student-area/student.js
+++ IqraCommerce/wwwroot/js/student-area/student.js
@@ -13,7 +13,7 @@ import { print } from "./student-form.js";
 
     function isValidPhoneNumber(phoneNumber) {
         // Regular expression to match the correct pattern of a Bangladeshi mobile number
-        const phoneNumberRegex = /^(?:\+?88)?01[15-9]\d{8}$/;
+        const phoneNumberRegex = /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/;
         return phoneNumberRegex.test(phoneNumber);
     }
 
